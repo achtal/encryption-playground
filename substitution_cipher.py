f5495ccd3ccff3c458c56e9151c16467deedb7a4
achtal <simon.therose@gmail.com>
2021-09-02 22:20:51 +0100

    initial commit
    substitution cipher from puzzle book

---
+++ substitution_cipher.py
@@ -0,0 +1,14 @@
+# simple cipher found in a puzzle book
+# encryption method:
+# swap last letter of word with first letter of following word for entire plain text
+# punctuation is ignored
+# case remains
+
+"""
+example:
+plain text
+The quick, brown fox jumps over the lazy dog
+
+encrypted
+Thq euicb, krowf noj xumpo svet rhl eazd yog
+"""
