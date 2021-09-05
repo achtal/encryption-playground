141452ba9ae1b94df72df74c24c9e68e641558e7
achtal <simon.therose@gmail.com>
2021-09-05 18:54:23 +0100

    base encryption build
    - need to handle punctuation properly

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -12,3 +12,23 @@ The quick, brown fox jumps over the lazy dog
 encrypted
 Thq euicb, krowf noj xumpo svet rhl eazd yog
 """
+
+
+def substitution_cipher(text):
+    cipher_text = ''
+    # break text into words
+    words = text.split(' ')
+    # loop through words and swap first and last letters
+    for i in range(len(words)):
+        if i == 0:
+            # first word only swaps last letter
+            new_word = words[i][:-1] + words[i+1][:1]
+            cipher_text += new_word
+        elif i == len(words) - 1:
+            new_word = ' ' + words[i-1][-1:] + words[i][1:]
+            cipher_text += new_word
+        else:
+            new_word = ' ' + words[i-1][-1:] + words[i][1:-1] + words[i+1][:1]
+            cipher_text += new_word
+    return cipher_text
+
