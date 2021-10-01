f867be02a32efe6077a2e9870a4334a67a1b42c9
achtal <86170006+achtal@users.noreply.github.com>
2021-10-01 14:31:34 +0100

    Update substitution_cipher.py
    keep original punctuation placement

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -12,12 +12,22 @@ The quick, brown fox jumps over the lazy dog
 encrypted
 Thq euicb, krowf noj xumpo svet rhl eazd yog
 """
+import string
 
 
 def substitution_cipher(text):
+    # handle punctuation in text
+    punct_dict = {}
+    # for punctuation found assign location to a dictionary
+    for x in string.punctuation:
+        punct_dict[x] = [n for n in range(len(text)) if text.find(x, n) == n]
+
+    # remove punctuation before applying cipher
+    text_no_punct = ''.join([char for char in text if char not in string.punctuation])
+    
     cipher_text = ''
     # break text into words
-    words = text.split(' ')
+    words = text_no_punct.split(' ')
     # loop through words and swap first and last letters
     for i in range(len(words)):
         if i == 0:
@@ -30,5 +40,11 @@ def substitution_cipher(text):
         else:
             new_word = ' ' + words[i-1][-1:] + words[i][1:-1] + words[i+1][:1]
             cipher_text += new_word
+            
+    # put punctuation back into string
+    for i in punct_dict.keys():
+        for place in punct_dict[i]:
+            cipher_text = cipher_text[:place] + i + cipher_text[place:]
+            
     return cipher_text
 
